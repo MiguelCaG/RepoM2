df5831727bf69e914ec67cb4d5812c6a59ae2e9e
MiguelCaG <m.carmonag.2020@alumnos.urjc.es>
2025-11-18 21:28:08 +0100

    Input System & Cinemachine Test Class

---
+++ FlappyBird/Assets/Scripts/InputSystem/PlayerController.cs
@@ -0,0 +1,110 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float playerSpeed = 2.0f;
+    public float jumpHeight = 1.0f;
+    public float gravityScale = -9.81f;
+
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool groundPlayer;
+    private Vector3 move;
+
+    private MyInputMap mIM;
+
+    private Vector2 inputData;
+
+    private bool isJumping = false;
+
+    public Transform cameraTransform;
+
+    public float charaterRotationSpeed = 10.0f;
+
+    private void Awake()
+    {
+        controller = gameObject.GetComponent<CharacterController>();
+
+        mIM = new MyInputMap();
+
+        mIM.PlayerWorld.Movement.performed += Movement_performed =>
+        {
+            inputData = Movement_performed.ReadValue<Vector2>();
+        };
+
+        mIM.PlayerWorld.Movement.canceled += Movement_canceled =>
+        {
+            inputData = Movement_canceled.ReadValue<Vector2>();
+        };
+
+        mIM.PlayerWorld.Jump.performed += Jump_performed =>
+        {
+            isJumping = Jump_performed.ReadValueAsButton();
+        };
+
+        mIM.PlayerWorld.Jump.canceled += Jump_canceled =>
+        {
+            isJumping = Jump_canceled.ReadValueAsButton();
+        };
+    }
+
+    private void OnEnable()
+    {
+        mIM.Enable();
+    }
+
+    private void OnDisable()
+    {
+        mIM.Disable();
+    }
+
+    private void Update()
+    {
+        groundPlayer = controller.isGrounded;
+
+        if (groundPlayer && playerVelocity.y < 0.0f)
+        {
+            playerVelocity.y = 0.0f;
+        }
+
+        if (cameraTransform != null)
+        {
+            Vector3 camForward = cameraTransform.forward;
+
+            camForward.y = 0.0f;
+            camForward.Normalize();
+
+            Vector3 camRight = cameraTransform.right;
+
+            camRight.y = 0.0f;
+            camRight.Normalize();
+
+            move = camForward * inputData.y + camRight * inputData.x;
+        }
+        else
+        {
+            move = transform.forward * inputData.y + transform.right * inputData.x;
+        }
+
+        move = Vector3.ClampMagnitude(move, 1f);
+
+        if (move.sqrMagnitude > 0.0001f)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(move, Vector3.up);
+            transform.rotation = Quaternion.Slerp(transform.rotation,
+            targetRotation, Time.deltaTime * charaterRotationSpeed);
+        }
+
+        if (isJumping && groundPlayer)
+        {
+            playerVelocity.y = Mathf.Sqrt(jumpHeight * -1.0f * gravityScale);
+        }
+
+        playerVelocity.y += gravityScale * Time.deltaTime;
+
+        Vector3 finalMove = (move * playerSpeed) + (playerVelocity.y * Vector3.up);
+        controller.Move(finalMove * Time.deltaTime);
+    }
+}
